b3284c82cfa73fc6fa6a2e11a7108f5d4c8f729b
Joel Christner <joel.christner@gmail.com>
2021-12-31 13:34:12 -0800

    NuGet v1.0.0.2, methods from database wrapper

--- ExpressionTree/Expr.cs
+++ ExpressionTree/Expr.cs
@@ -63,6 +63,20 @@ namespace ExpressionTree
             Right = right;
         }
 
+        /// <summary>
+        /// An expression that allows you to determine if an object is between two values, i.e. GreaterThanOrEqualTo the first value, and LessThanOrEqualTo the second value.
+        /// </summary>
+        /// <param name="left">The left term of the expression; can either be a string term or a nested expression.</param> 
+        /// <param name="right">List of two values where the first value is the lower value and the second value is the higher value.</param>
+        public static Expr Between(object left, List<object> right)
+        {
+            if (right == null) throw new ArgumentNullException(nameof(right));
+            if (right.Count != 2) throw new ArgumentException("Right term must contain exactly two members.");
+            Expr startOfBetween = new Expr(left, OperatorEnum.GreaterThanOrEqualTo, right.First());
+            Expr endOfBetween = new Expr(left, OperatorEnum.LessThanOrEqualTo, right.Last());
+            return PrependAndClause(startOfBetween, endOfBetween);
+        }
+
         #endregion
 
         #region Public-Methods
@@ -128,6 +142,208 @@ namespace ExpressionTree
 
             ret += ")";
             return ret;
+        }
+
+        /// <summary>
+        /// Prepends a new expression using the supplied left term, operator, and right term using an AND clause.
+        /// </summary>
+        /// <param name="left">The left term of the expression; can either be a string term or a nested expression.</param>
+        /// <param name="oper">The operator.</param>
+        /// <param name="right">The right term of the expression; can either be an object for comparison or a nested expression.</param>
+        public void PrependAnd(object left, OperatorEnum oper, object right)
+        {
+            Expr e = new Expr(left, oper, right);
+            PrependAnd(e);
+        }
+
+        /// <summary>
+        /// Prepends the expression with the supplied expression using an AND clause.
+        /// </summary>
+        /// <param name="prepend">The expression to prepend.</param> 
+        public void PrependAnd(Expr prepend)
+        {
+            if (prepend == null) throw new ArgumentNullException(nameof(prepend));
+
+            Expr orig = new Expr(this.Left, this.Operator, this.Right);
+            Expr e = PrependAndClause(prepend, orig);
+            Left = e.Left;
+            Operator = e.Operator;
+            Right = e.Right;
+        }
+
+        /// <summary>
+        /// Prepends a new expression using the supplied left term, operator, and right term using an OR clause.
+        /// </summary>
+        /// <param name="left">The left term of the expression; can either be a string term or a nested expression.</param>
+        /// <param name="oper">The operator.</param>
+        /// <param name="right">The right term of the expression; can either be an object for comparison or a nested expression.</param>
+        public void PrependOr(object left, OperatorEnum oper, object right)
+        {
+            Expr e = new Expr(left, oper, right);
+            PrependOr(e);
+        }
+
+        /// <summary>
+        /// Prepends the expression with the supplied expression using an OR clause.
+        /// </summary>
+        /// <param name="prepend">The expression to prepend.</param> 
+        public void PrependOr(Expr prepend)
+        {
+            if (prepend == null) throw new ArgumentNullException(nameof(prepend));
+
+            Expr orig = new Expr(this.Left, this.Operator, this.Right);
+            Expr e = PrependOrClause(prepend, orig);
+            Left = e.Left;
+            Operator = e.Operator;
+            Right = e.Right;
+
+            return;
+        }
+
+        /// <summary>
+        /// Prepends the expression in prepend to the expression original using an AND clause.
+        /// </summary>
+        /// <param name="prepend">The expression to prepend.</param>
+        /// <param name="original">The original expression.</param>
+        /// <returns>A new expression.</returns>
+        public static Expr PrependAndClause(Expr prepend, Expr original)
+        {
+            if (prepend == null) throw new ArgumentNullException(nameof(prepend));
+            if (original == null) throw new ArgumentNullException(nameof(original));
+            Expr ret = new Expr
+            {
+                Left = prepend,
+                Operator = OperatorEnum.And,
+                Right = original
+            };
+            return ret;
+        }
+
+        /// <summary>
+        /// Prepends the expression in prepend to the expression original using an OR clause.
+        /// </summary>
+        /// <param name="prepend">The expression to prepend.</param>
+        /// <param name="original">The original expression.</param>
+        /// <returns>A new expression.</returns>
+        public static Expr PrependOrClause(Expr prepend, Expr original)
+        {
+            if (prepend == null) throw new ArgumentNullException(nameof(prepend));
+            if (original == null) throw new ArgumentNullException(nameof(original));
+            Expr ret = new Expr
+            {
+                Left = prepend,
+                Operator = OperatorEnum.Or,
+                Right = original
+            };
+            return ret;
+        }
+
+        /// <summary>
+        /// Convert a list of expression objects to a nested expression containing AND between each expression in the list. 
+        /// </summary>
+        /// <param name="exprList">List of expression objects.</param>
+        /// <returns>A nested expression.</returns>
+        public static Expr ListToNestedAndExpression(List<Expr> exprList)
+        {
+            if (exprList == null) throw new ArgumentNullException(nameof(exprList));
+            if (exprList.Count < 1) return null;
+
+            int evaluated = 0;
+            Expr ret = null;
+            Expr left = null;
+            List<Expr> remainder = new List<Expr>();
+
+            if (exprList.Count == 1)
+            {
+                foreach (Expr curr in exprList)
+                {
+                    ret = curr;
+                    break;
+                }
+
+                return ret;
+            }
+            else
+            {
+                foreach (Expr curr in exprList)
+                {
+                    if (evaluated == 0)
+                    {
+                        left = new Expr();
+                        left.Left = curr.Left;
+                        left.Operator = curr.Operator;
+                        left.Right = curr.Right;
+                        evaluated++;
+                    }
+                    else
+                    {
+                        remainder.Add(curr);
+                        evaluated++;
+                    }
+                }
+
+                ret = new Expr();
+                ret.Left = left;
+                ret.Operator = OperatorEnum.And;
+                Expr right = ListToNestedAndExpression(remainder);
+                ret.Right = right;
+
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Convert a list of expression objects to a nested expression containing OR between each expression in the list. 
+        /// </summary>
+        /// <param name="exprList">List of expression objects.</param>
+        /// <returns>A nested expression.</returns>
+        public static Expr ListToNestedOrExpression(List<Expr> exprList)
+        {
+            if (exprList == null) throw new ArgumentNullException(nameof(exprList));
+            if (exprList.Count < 1) return null;
+
+            int evaluated = 0;
+            Expr ret = null;
+            Expr left = null;
+            List<Expr> remainder = new List<Expr>();
+
+            if (exprList.Count == 1)
+            {
+                foreach (Expr curr in exprList)
+                {
+                    ret = curr;
+                    break;
+                }
+
+                return ret;
+            }
+            else
+            {
+                foreach (Expr curr in exprList)
+                {
+                    if (evaluated == 0)
+                    {
+                        left = new Expr();
+                        left.Left = curr.Left;
+                        left.Operator = curr.Operator;
+                        left.Right = curr.Right;
+                        evaluated++;
+                    }
+                    else
+                    {
+                        remainder.Add(curr);
+                        evaluated++;
+                    }
+                }
+
+                ret = new Expr();
+                ret.Left = left;
+                ret.Operator = OperatorEnum.Or;
+                Expr right = ListToNestedOrExpression(remainder);
+                ret.Right = right;
+
+                return ret;
+            }
         }
 
         #endregion
